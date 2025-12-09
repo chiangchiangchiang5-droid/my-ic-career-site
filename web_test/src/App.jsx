1ab6ca78a3ae4de995cc8dc2b05844c1965fd94b
weichen <chiangchiangchiang5@gmail.com>
2025-12-10 00:23:40 +0800

    Fix Syntax Error and Netlify 404 Redirect

--- web_test/src/App.jsx
+++ web_test/src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 // 引入元件
-import Navbar from './components/Navbar';
+// 從這裡: import Navbar from './src/components/Navbar.jsx';
+// 改為這樣 (使用大括號):
+import { Navbar } from './src/components/Navbar.jsx';
 import Home from './pages/Home';
 import ICIntro from './pages/ICIntro';
 import Salary from './pages/Salary';
